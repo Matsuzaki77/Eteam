6da8f11a07801f3f7eff5659856249ba5bac5e28
Nagata0110 <115524429+Nagata0110@users.noreply.github.com>
2022-10-25 13:42:32 +0900

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,5 +1,6 @@
 <?php
 try{
+    //データベース接続
     $pdo=new PDO('mysql:host=mysql207.phy.lolipop.lan;dbname=LAA1418446-sys2022;charset=utf8','LAA1418446', 'Eaiueo1234');
     echo '接続成功';
 }catch(PDOException $e){
