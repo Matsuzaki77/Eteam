8246d95145b351af2bd56cd790db4c3f9130c7bf
Nagata0110 <115524429+Nagata0110@users.noreply.github.com>
2022-11-02 09:32:28 +0900

    Update shohin4.php

--- shohin/shohin4.php
+++ shohin/shohin4.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>shohin1</title>
+    <title>shohin4</title>
     
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -13,23 +13,22 @@
 </head>
 <body>
 <div name ="maindiv" class="container">
-    <nav class="navbar navbar-expand-md navbar-dark" aria-label="Fourth navbar example" style="background-color: #f7a10c;">
+<nav class="navbar navbar-expand-md navbar-dark" aria-label="Fourth navbar example" style="background-color: #f7a10c;">
         <div class="container-fluid">
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-    
           <div class="collapse navbar-collapse" id="navbarsExample04">
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">トップ</a>
+                <a class="nav-link active" aria-current="page" href="../shohin.php">トップ</a>
               </li>
             </ul>
           </div>
         </div>
 
 <div class="container-fluid">
-        <div class="offset-10 col-4">
+        <div class="offset-8 col-4">
     <?php
                 session_start();
                 if(isset($_SESSION['name']) == false || isset($_SESSION['id']) == false ){
@@ -66,7 +65,7 @@
                         温度帯： 冷凍</p> 
                     </div>
                 </div>
-            </div>
+                </div>
         </div>
     </div>
     <!-- <div class="row">           
