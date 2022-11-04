ebba54f3d9988ebeede21b0c830511405e0c3b5f
Nagata0110 <115524429+Nagata0110@users.noreply.github.com>
2022-11-04 11:10:55 +0900

    Update shohin4.php

--- shohin/shohin4.php
+++ shohin/shohin4.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>shohin4</title>
+    <title>よくばり冷凍 完熟マンゴーセット</title>
     
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -23,21 +23,22 @@
                 <a class="nav-link active" aria-current="page" href="../shohin.php">トップ</a>
               </li>
             </ul>
+	
           </div>
-        </div>
-
-<div class="container-fluid">
-        <div class="offset-8 col-4">
-              <?php
+<div class="row col-lg-2 col-md-2 col-4">
+<?php
                 session_start();
                 if(isset($_SESSION['name']) == false || isset($_SESSION['id']) == false ){
 	                header('Location:../login.php');
                 }
 
                 echo "ようこそ".$_SESSION['name']."さん<br>";
-                echo '<a href="logout.php">ログアウト</a>';
-              ?>
+                echo '<a href="../logout.php">ログアウト</a>';
+            ?>
+</div>
         </div>
+        
+    
 </div>
 
       </nav>
@@ -47,26 +48,25 @@
         <div class="col-sm-4 col-12 mt-3">
             <img src="../img/mango.jpg" class="img-fluid">
         </div>
-        <div class="col-sm-8 col-12 mt-3">
+        <div class="col-sm-8 mt-3 col-12">
             <h3>よくばり冷凍 完熟マンゴーセット 1kg(500g×2袋)</h3>
             <h5 class="mt-2 col-12">価格:2,890円</h5>
-            <select name="cnt">
+数量：<select name="cnt">
               <option value="1" selected>1</option>
               <option value="2">2</option>
               <option value="3">3</option>
               <option value="4">4</option>
               <option value="5">5</option>
             </select>
-
             <div class="row">
                 <div class="col-sm-6 col-12 mt-2">
                     <button class="btn-sm btn-warning text-white">カートに入れる</button>
                 </div>
                 <div class="row">
-                    <div class="col-sm-12 col-12 mt-3">
+                    <div class="col-sm-12 mt-3 col-12">
                         <h5>商品詳細</h5>
                         <p>内容量：
-                        よくばり 冷凍完熟マンゴーセット 1kg(500g×2袋)
+                        よくばり 冷凍完熟マンゴーセット 1kg(500g×2袋)<br>
                         【内容量】1kg(500g×2袋)<br>
                         【保存方法】　冷凍<br>
                         【賞味期限】　製造より2年<br>
