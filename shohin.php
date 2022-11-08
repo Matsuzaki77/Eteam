6c7d5c9ebb1bf5e745baca8e51b4540b1f2f5ded
Matsuzaki77 <115523483+Matsuzaki77@users.noreply.github.com>
2022-11-08 14:29:07 +0900

    アイコン

--- shohin.php
+++ shohin.php
@@ -15,6 +15,7 @@ background-position: center 60%;
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+    <link rel="shortcut icon" href="./img/favicon.ico">
 </head>
 <body>
 <nav class="navbar navbar-expand-md navbar-dark" aria-label="Fourth navbar example" style="background-color: #f7a10c;">
