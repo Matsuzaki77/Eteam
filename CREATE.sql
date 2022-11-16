6bbc87f5ec4935dbbdb3c7de10734142a9886501
Nagata0110 <115524429+Nagata0110@users.noreply.github.com>
2022-11-16 11:12:33 +0900

    Update CREATE.sql

--- CREATE.sql
+++ CREATE.sql
@@ -1,4 +1,4 @@
---1 顧客情報 
+
 CREATE TABLE users 
 (user_id VARCHAR(100) NOT NULL, 
 user_name VARCHAR(50) NOT NULL, 
@@ -8,18 +8,19 @@ PRIMARY KEY(user_id)
 ); 
 
  
---2 商品情報 
+
 CREATE TABLE products 
 (product_id VARCHAR(100) NOT NULL, 
  product_name VARCHAR(100) NOT NULL, 
  product_price INT(10) NOT NULL, 
  product_img VARCHAR(100) NOT NULL,
+ product_detail VARCHAR(800) NOT NULL,
 PRIMARY KEY(product_id) 
 ); 
 
---3 カート 
+
 CREATE TABLE carts 
-(cart_id INT(10) NOT NULL, 
+(cart_id INT AUTO_INCREMENT NOT NULL, 
 user_id VARCHAR(100) NOT NULL,
 PRIMARY KEY(cart_id), 
 FOREIGN KEY(user_id) REFERENCES users (user_id) 
@@ -27,9 +28,9 @@ FOREIGN KEY(user_id) REFERENCES users (user_id)
 
  
 
---4 カート詳細 
+
 CREATE TABLE cart_details 
-(cart_id INT(10) NOT NULL, 
+(cart_id INT AUTO_INCREMENT NOT NULL, 
  product_id VARCHAR(100) NOT NULL, 
 Shohin_quanity INT(10) NOT NULL, 
 PRIMARY KEY(cart_id,product_id), 
@@ -37,10 +38,10 @@ FOREIGN KEY(cart_id) REFERENCES carts (cart_id),
 FOREIGN KEY(product_id) REFERENCES products (product_id) 
 ); 
 
---5 注文 
+ 
 CREATE TABLE solds  
-(sold_id INT(10) NOT NULL, 
- cart_id INT(10) NOT NULL, 
+(sold_id INT AUTO_INCREMENT NOT NULL, 
+ cart_id INT AUTO_INCREMENT NOT NULL, 
  user_id VARCHAR(100) NOT NULL, 
 product_id VARCHAR(100)NOT NULL, 
 order_date DATE NOT NULL, 
@@ -50,10 +51,14 @@ FOREIGN KEY(cart_id) REFERENCES cart_details  (cart_id),
 FOREIGN KEY(product_id)REFERENCES cart_details (product_id) 
 ); 
 
-INSERT INTO products(product_id,product_name,product_price,product_img)VALUES("1","熊本馬刺し５種食べ比べセット250ｇ（約5人前）",6980,"basashi.jpg"); 
-INSERT INTO products(product_id,product_name,product_price,product_img)VALUES("2","一番人気のカニ定番品700g（総重量1kg）",5970,"kani.jpg"); 
-INSERT INTO products(product_id,product_name,product_price,product_img)VALUES("3","特典付き国産養殖とらふぐ・セット(3-4人前)送料無料・冷凍",13980,"fugu.jpg"); 
-INSERT INTO products(product_id,product_name,product_price,product_img)VALUES("4","よくばり冷凍 完熟マンゴーセット 1kg(500g×2袋) ",2890,"mango.jpg"); 
+INSERT INTO products(product_id,product_name,product_price,product_img,product_detail)VALUES("1","熊本馬刺し５種食べ比べセット250ｇ（約5人前）",6980,"basashi.jpg",
+"・赤身馬刺し（約50g）1パック\n・霜降り（中トロ）馬刺し（約50g）1パック\n・大トロ馬刺し（約50g）1パック\n・たてがみ刺し（約50g）1パック\n・馬ヒレ 馬刺し用（約50g）1パック"); 
+INSERT INTO products(product_id,product_name,product_price,product_img,product_detail)VALUES("2","一番人気のカニ定番品700g（総重量1kg）",5970,"kani.jpg","
+・一番人気の定番品700g 総重量1kg\n・特大棒ポーション400g 総重量500g\n・特大爪/爪下/肩肉800g 総重量1.1kg"); 
+INSERT INTO products(product_id,product_name,product_price,product_img,product_detail)VALUES("3","特典付き国産養殖とらふぐ・セット(3-4人前)送料無料・冷凍",13980,"fugu.jpg",
+"・とらふぐ刺身 約100g\n・鍋用上身 約200g\n・鍋用アラ 約400g\n・選べる特典品1点\n・ふぐ皮湯引き 約80g\n・ツミレ 8個\n・フグヒレ 2～3枚\n・ポン酢、薬味"); 
+INSERT INTO products(product_id,product_name,product_price,product_img,product_detail)VALUES("4","よくばり冷凍 完熟マンゴーセット 1kg(500g×2袋) ",2890,"mango.jpg",
+"よくばり 冷凍完熟マンゴーセット 1kg(500g×2袋)\n【内容量】1kg(500g×2袋)\n【保存方法】　冷凍\n【賞味期限】　製造より2年\nマンゴーの種類： その他マンゴー\n温度帯： 冷凍"); 
 
 INSERT INTO users(user_id,user_name,user_pass,user_address)VALUES("2101201@s.asojuku.ac.jp","藤元","2101201","福岡県筑紫野市");
 INSERT INTO users(user_id,user_name,user_pass,user_address)VALUES("2101188@s.asojuku.ac.jp","永田","2101188","福岡県小郡市");
@@ -62,3 +67,4 @@ INSERT INTO users(user_id,user_name,user_pass,user_address)VALUES("2101208@s.aso
 INSERT INTO users(user_id,user_name,user_pass,user_address)VALUES("2101236@s.asojuku.ac.jp","米本","2101236","山口県山口市");
 
 --ALTER TABLE products ADD COLUMN product_image VARCHAR(100); 追加の列
+--ALTER TABLE products ADD COLUMN product_detail VARCHAR(800) NOT NULL;
