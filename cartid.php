8636de009b76fb6e41a74fce7b97edad2ad00d1c
Nagata0110 <115524429+Nagata0110@users.noreply.github.com>
2022-11-25 12:27:11 +0900

    Update cartid.php

--- cartid.php
+++ cartid.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
-    $pdo = new PDO('mysql:host=localhost;dbname=usertbl;charset=utf8','webuser','abccsd2');
+    //$pdo = new PDO('mysql:host=localhost;dbname=usertbl;charset=utf8','webuser','abccsd2');
+    $pdo=new PDO('mysql:host=mysql207.phy.lolipop.lan;dbname=LAA1418446-sys2022;charset=utf8','LAA1418446', 'Eaiueo1234');
     $sqlcart = "INSERT INTO carts(user_id)VALUES(?)";
     $ps = $pdo -> prepare($sqlcart);
     $ps -> bindValue(1,$_SESSION['id'],PDO::PARAM_STR);
