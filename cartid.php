56f2fc29aaf5ee2139cf58a0baa6ed6bf93ad8ff
Nagata0110 <115524429+Nagata0110@users.noreply.github.com>
2022-11-25 11:52:50 +0900

    カートid　付与

---
+++ cartid.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+    $pdo = new PDO('mysql:host=localhost;dbname=usertbl;charset=utf8','webuser','abccsd2');
+    $sqlcart = "INSERT INTO carts(user_id)VALUES(?)";
+    $ps = $pdo -> prepare($sqlcart);
+    $ps -> bindValue(1,$_SESSION['id'],PDO::PARAM_STR);
+    $ps -> execute();
+
+    $selectcart = "SELECT * FROM carts WHERE user_id = ?";
+    $ps = $pdo -> prepare($selectcart);
+    $ps->bindValue(1, $_SESSION['id'], PDO::PARAM_STR);
+    $ps->execute();
+    foreach($ps -> fetchAll() as $row){
+        $_SESSION['cart_id'] = $row['cart_id'];
+        header('Location:shohin.php');
+    }
+?>
