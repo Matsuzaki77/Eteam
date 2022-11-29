207c2f2f28bd2d0d9c3104a6486840bab06a757f
fujimondesu <2101201@s.asojuku.ac.jp>
2022-11-29 14:37:40 +0900

    Update cart-delete.php

--- cart/cart-delete.php
+++ cart/cart-delete.php
@@ -1,54 +1,22 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>カート詳細</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
-    <link rel="shortcut icon" href="../img/favicon.ico">
-</head>
-
-<body>
-<nav class="navbar navbar-expand-md navbar-dark" aria-label="Fourth navbar example" style="background-color: #f7a10c;">
-        <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarsExample04">
-            <ul class="navbar-nav me-auto mb-2 mb-md-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="../menu.php">トップ</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="./cart.html"><i class="bi bi-cart"></i>カート</a>
-              </li>
-            </ul>
-	
-          </div>
-          <div class="row col-lg-2 col-md-2 col-4">
-              <?php
-                session_start();
-                if(isset($_SESSION['name']) == false || isset($_SESSION['id']) == false ){
-	                header('Location:../login.php');
-                }
-
-                echo "ようこそ".$_SESSION['name']."さん<br>";
-                echo '<a href="../logout.php">ログアウト</a>';
-              ?>
-          </div>
-        </div>
-</nav>
-
-<?php require '../cart.php';?>
-<?php require 'menu.php';?>
 <?php
+$pdo=new PDO('mysql:host=mysql207.phy.lolipop.lan;dbname=LAA1418446-sys2022;charset=utf8','LAA1418446', 'Eaiueo1234');
+$ps = $pdo -> prepare($sql);
+if(isset($_POST["id"])){
+  $sql = "DELETE FROM products WHERE product_id = ?";
+  $ps = $pdo->prepare($sql);
+  $ps->bindValue(1, $_POST['id'], PDO::PARAM_INT);
+  $ps->execute();
+
 session_start();
 unset($_SESSION['products'][$_REQUEST['product_id']]);
-echo 'カートから商品を削除しました。';
+echo '<h3>カートから商品を削除しました。</h3>';
 echo '<hr>';
-require 'cart.php';
+}
+
+$selectSQL = "SELECT * FROM products";
+$selectdata = $pdo->query($selectSQL);
+foreach($selectdata as $row){
+  echo "$row[product_name]:$row[product_price]<br>";
+  }
+  
 ?>
-<?php require '../footer.php';?>
