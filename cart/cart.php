3884cb347fc9773c2d5729106ca946ae3198dfb1
Nagata0110 <115524429+Nagata0110@users.noreply.github.com>
2022-11-29 14:25:36 +0900

    Update cart.php

--- cart/cart.php
+++ cart/cart.php
@@ -20,7 +20,7 @@
           <div class="collapse navbar-collapse" id="navbarsExample04">
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="../menu.php">トップ</a>
+                <a class="nav-link active" aria-current="page" href="../shohin.php">トップ</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="./cart.html"><i class="bi bi-cart"></i>カート</a>
@@ -30,13 +30,13 @@
           </div>
           <div class="row col-lg-2 col-md-2 col-4">
               <?php
-                // session_start();
-                // if(isset($_SESSION['name']) == false || isset($_SESSION['id']) == false ){
-	              //   header('Location:../login.php');
-                // }
+                session_start();
+                if(isset($_SESSION['name']) == false || isset($_SESSION['id']) == false ){
+	                header('Location:../login.php');
+                }
 
-                // echo "ようこそ".$_SESSION['name']."さん<br>";
-                // echo '<a href="../logout.php">ログアウト</a>';
+                echo "ようこそ".$_SESSION['name']."さん<br>";
+                echo '<a href="../logout.php">ログアウト</a>';
               ?>
           </div>
         </div>
@@ -44,16 +44,25 @@
 
 <div name ="maindiv" class="container-fluid">
   <div class="row">
+  <h2 class="mt-5">ショッピングカート</h2>
     <?php
+    //$pdo = new PDO('mysql:host=localhost;dbname=usertbl;charset=utf8','webuser','abccsd2');
     $pdo=new PDO('mysql:host=mysql207.phy.lolipop.lan;dbname=LAA1418446-sys2022;charset=utf8','LAA1418446', 'Eaiueo1234');
-    $sql = "SELECT * FROM products WHERE product_id = 3";
-    //$ps -> bindValue(1,$_SESSION['product_id'],PDO::PARAM_STR);
+    $sql = "SELECT C.user_id,Cd.cart_id,P.product_id,P.product_img,P.product_price,Cd.Shohin_quanity,P.product_name
+            FROM carts AS C INNER JOIN cart_details AS Cd
+            ON C.cart_id = Cd.cart_id
+            INNER JOIN products AS P
+            ON Cd.product_id = P.product_id
+            WHERE Cd.cart_id = ?";
     $ps = $pdo -> prepare($sql);
+    $ps -> bindValue(1,$_SESSION['cart_id'],PDO::PARAM_INT);
     $ps -> execute();
-    foreach($ps -> fetchAll() as $row){
+    $selectArray = $ps -> fetchAll();
+    $sum = 0;
+
+    foreach($selectArray as $row){
     echo
-    '<div class="col-sm-8 col-12 mt-5">
-      <h2>ショッピングカート</h2>
+    '<div class="col-sm-8 col-12 mt-2">
       <!--カートに入れた商品を表示-->
       <div class="row mt-2">
         <div class="col-6">
@@ -62,17 +71,20 @@
         <div class="col-6">
           <b>'.$row['product_name'].'</b>
           <p>価格:'.number_format($row['product_price']).'円<br>
-          数量：'.$_POST['cnt'].' <button class="btn-sm btn-danger ms-4 mt-3 mb-1 ">削除</button>';
-          $sum += $_POST['cnt'] * $row['product_price'];
+          数量：'.$row['Shohin_quanity'].' <button class="btn-sm btn-danger ms-4 mt-3 mb-1 ">削除</button>';
           echo
         '</div>
       </div>
-    </div>
-    <div class="col-sm-3 text-end">
-      <h2 class="mt-5">合計：'.number_format($sum).'円</h2>
-      <button class="btn-lg btn-warning mt-3"><b>注文確定</b></button>
+      <hr>
     </div>';
     }
+    echo
+    '<div class="col-sm-3 text-end">';
+    $sum += $row['Shohin_quanity'] * $row['product_price'];
+    echo
+      '<h2 class="mt-5">合計：'.number_format($sum).'円</h2>
+      <button class="btn-lg btn-warning mt-3"><b>注文確定</b></button>
+    </div>';
     ?>
   </div>
 </div>
