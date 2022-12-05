4c1baf78219394f0f6a167a2177c4414d57ed7ba
Nagata0110 <115524429+Nagata0110@users.noreply.github.com>
2022-12-05 09:34:32 +0900

    Update cart-delete.php

--- cart/cart-delete.php
+++ cart/cart-delete.php
@@ -1,11 +1,11 @@
 <?php
 $pdo=new PDO('mysql:host=mysql207.phy.lolipop.lan;dbname=LAA1418446-sys2022;charset=utf8','LAA1418446', 'Eaiueo1234');
 
-  $sql = "DELETE * FROM cart_details WHERE product_id = ? AND cart_id = ?";
-  $ps = $pdo->prepare($sql);
-  $ps->bindValue(1, $_POST['product_id'], PDO::PARAM_STR);
-  $ps->bindValue(2, $_POST['cart_id'], PDO::PARAM_INT);
-  $ps->execute();
+  $sql = "DELETE FROM cart_details WHERE cart_id = ? AND product_id = ?";
+    $ps = $pdo->prepare($sql);
+    $ps->bindValue(1, $_POST['cart_id'], PDO::PARAM_INT);
+    $ps->bindValue(2, $_POST['product_id'], PDO::PARAM_STR);
+    $ps->execute();
 
 echo '<h3>カートから商品を削除しました。</h3><br>';
 echo '<a href="./cart.php">カート画面</a>';
