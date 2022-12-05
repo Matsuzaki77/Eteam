dd416b2489b33ed2eb339cdd1fe7b133a24c9cde
Nagata0110 <115524429+Nagata0110@users.noreply.github.com>
2022-12-05 10:18:22 +0900

    Update menu.php

--- menu.php
+++ menu.php
@@ -32,9 +32,7 @@ background-position: center 60%;
                 <a class="nav-link active" aria-current="page" href="./cart/cart.php"><i class="bi bi-cart"></i>カート</a>
               </li>
             </ul>
-	
-          </div>
-          <div class="row col-lg-2 col-md-2 col-4">
+            <div class="row col-lg-2 col-md-2 col-4">
               <?php
                 session_start();
                 if(isset($_SESSION['name']) == false || isset($_SESSION['id']) == false ){
@@ -44,6 +42,7 @@ background-position: center 60%;
                 echo "ようこそ".$_SESSION['name']."さん<br>";
                 echo '<a href="./logout.php">ログアウト</a>';
               ?>
+          </div>
           </div>
         </div>
 </nav>
