21dd409ce161928a784d5bf102cd7795e8b0a052
Nagata0110 <115524429+Nagata0110@users.noreply.github.com>
2022-12-05 11:20:17 +0900

    Update cart-delete.php

--- cart/cart-delete.php
+++ cart/cart-delete.php
@@ -6,8 +6,5 @@ $pdo=new PDO('mysql:host=mysql207.phy.lolipop.lan;dbname=LAA1418446-sys2022;char
     $ps->bindValue(1, $_POST['cart_id'], PDO::PARAM_INT);
     $ps->bindValue(2, $_POST['product_id'], PDO::PARAM_STR);
     $ps->execute();
-
-echo '<h3>カートから商品を削除しました。</h3><br>';
-echo '<a href="./cart.php">カート画面</a>';
-echo '<hr>';
+    header('Location:cart.php');
 ?>
