0c5cc22b8f357bf6b3fa26e56e3d15235c589c00
Nagata0110 <115524429+Nagata0110@users.noreply.github.com>
2022-12-06 14:24:54 +0900

    Update sold.php

--- cart/sold.php
+++ cart/sold.php
@@ -42,10 +42,10 @@ $ps->execute();
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active text-white" aria-current="page" href="#"></a>
+                
               </li>
               <li class="nav-item">
-                <a class="nav-link active text-white" href="#"></a>
+                
               </li>
               <li class="nav-item d-sm-none">
               <?php
