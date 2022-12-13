8599792e18a5c1b77a8fd4da7332447b1fdecad1
fujimondesu <2101201@s.asojuku.ac.jp>
2022-12-13 10:51:46 +0900

    Update CREATE.sql

--- CREATE.sql
+++ CREATE.sql
@@ -56,13 +56,18 @@ INSERT INTO products(product_id,product_name,product_price,product_img,product_d
 INSERT INTO products(product_id,product_name,product_price,product_img,product_detail)VALUES("3","特典付き国産養殖とらふぐ・セット(3-4人前)送料無料・冷凍",13980,"fugu.jpg",
 "・とらふぐ刺身 約100g\n・鍋用上身 約200g\n・鍋用アラ 約400g\n・選べる特典品1点\n・ふぐ皮湯引き 約80g\n・ツミレ 8個\n・フグヒレ 2～3枚\n・ポン酢、薬味"); 
 INSERT INTO products(product_id,product_name,product_price,product_img,product_detail)VALUES("4","よくばり冷凍 完熟マンゴーセット 1kg(500g×2袋) ",2890,"mango.jpg",
-"よくばり 冷凍完熟マンゴーセット 1kg(500g×2袋)\n【内容量】1kg(500g×2袋)\n【保存方法】　冷凍\n【賞味期限】　製造より2年\nマンゴーの種類： その他マンゴー\n温度帯： 冷凍"); 
+"よくばり 冷凍完熟マンゴーセット 1kg(500g×2袋)\n【内容量】1kg(500g×2袋)\n【保存方法】　冷凍\n【賞味期限】　製造より2年\nマンゴーの種類： その他マンゴー\n温度帯： 冷凍");
 INSERT INTO products(product_id,product_name,product_price,product_img,product_detail)VALUES("5","かぼすぶり丼 大分県産 季節限定かぼすぶり丼 70g×4袋セット",3480,"buri.jpg",
 "商品名：かぼすぶり丼\n名称：海産物加工品\n内容量：かぼすぶり丼 70g×4袋\n賞味期限：パッケージに記載（約90日）\n保存方法：冷凍(-18度以下)で保存してください。\nお召し上がり方：流水で解凍し、温かいご飯にかければ出来上がりです。\n区分：食品");
+<<<<<<< HEAD
 
 INSERT INTO products(product_id,product_name,product_price,product_img,product_detail)VALUES("7","厳選トマト 桃太郎 約4kg",3980,"tomato.jpg",
 "商品名：桃太郎トマト\n重量(箱込み)：約4kg\n保存方法：冷蔵庫や冷暗所などの涼しい場所で保存して下さい。\n賞味期限:4日前後目安");
 
+=======
+INSERT INTO products(product_id,product_name,product_price,product_img,product_detail)VALUES("6","広島風お好み焼き (豚玉そば入り・イカ天入り)　お試し３枚セット",3500,"okonomiyaki.jpg",
+"お好み焼き　そば入り500ｇ前後(特製ソース50ｇ・青のり0.5ｇ含む)×3\n広島風お好み焼きは、袋のまま切れ目を入れ、電子レンジで温めてください。\n５００Ｗで約９分３０秒　６００Ｗで約８分３０秒");
+>>>>>>> df58c0dcf5b6c2439bc299f263aad199bdc9ad97
 INSERT INTO products(product_id,product_name,product_price,product_img,product_detail)VALUES("","",,"","");
 
 INSERT INTO users(user_id,user_name,user_pass,user_address)VALUES("2101201@s.asojuku.ac.jp","藤元","2101201","福岡県筑紫野市");
