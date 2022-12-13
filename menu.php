ad79842d9993f8f75f6a2386774ba289a0df72f9
Nagata0110 <115524429+Nagata0110@users.noreply.github.com>
2022-12-13 10:05:46 +0900

    Update menu.php

--- menu.php
+++ menu.php
@@ -15,6 +15,7 @@ background-position: center 60%;
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="./css/style.css" />
     <link rel="shortcut icon" href="./img/favicon.ico">
 </head>
 <body>
@@ -83,12 +84,13 @@ background-position: center 60%;
                   $id = $row['product_id']; ?>
                   <div class="col-lg-3 col-md-6">
                       <div class="card">
-                          <a href="./shohin/shohin.php?id=<?php echo $id ?>"><img class="card-img-top" src="img/<?php echo $row['product_img'] ?>"></a>
+                      <a href="./shohin/shohin.php?id=<?php echo $id ?>" class="link">
+                          <img class="card-img-top" src="img/<?php echo $row['product_img'] ?>">
                           <div class="card-body">
                               <h5 class="card-title"><?php echo $row['product_name'] ?></h5>
                               <p class="text-danger text-uppercase">￥<?php echo number_format($row['product_price']) ?></p>
-                              <a href="./shohin/shohin.php?id=<?php echo $id ?>">>>>商品詳細へ</a>
                           </div>
+                      </a>
                       </div>
                   </div>
           <?php
